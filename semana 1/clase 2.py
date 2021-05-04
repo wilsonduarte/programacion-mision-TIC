cd9eb7f3caeffcfed2b205a59f42d766f01d531d
wilsonduarte <wreportv@hotmail.com>
2021-05-03 19:38:47 -0500

    taller semana 1

---
+++ semana 1/clase 2.py
@@ -0,0 +1,44 @@
+print()
+print("hola mundo")
+print(3423+24242*5424)
+# wilson duarte
+print("comentarios")
+print("enteros =" , 7 , type(7)) 
+range(1,19)
+variable_1 = 5+5
+variable_2 = variable_1 + 5
+variable_3 = variable_1 + variable_2
+print(variable_3)
+print(12//0.2)
+print(11**2)
+modulo = 110
+modulo %=0.2
+print(modulo)
+print(13 - 4 * (5 - 2) + 3 * (2 + 8))
+print(16 + 3 * (6 - 4) - 3 * 5)
+
+# ejercicio peso
+print(5.95 * 10)
+print(5.95 * 1000 // 100)
+
+peso_empaque = 100
+precio_empaque = 5.95
+peso_kilo = 1000
+precio_gramo = precio_empaque / peso_empaque
+precio_kilo = peso_kilo * precio_gramo
+
+print("el precio por kilo es", precio_kilo)
+print()
+print("EJERCICIO 3")
+
+peces_rojos = 284
+peces_azules = 163
+total_peces = peces_rojos + peces_azules
+print("El total de peces es", total_peces)
+print('''
+
+
+
+
+''')
+print(3+3)
